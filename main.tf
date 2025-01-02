9ef70c5ac7558541c875bd7ffa600d48d8b8ea1e
maximillianus <pradana.aditya@gmail.com>
2025-01-03 01:11:25 +0700

    test HCP Terraform Integration #5 - tidying workflows, add ec2

--- main.tf
+++ main.tf
@@ -32,15 +32,15 @@ resource "terraform_data" "run-script" {
 
 }
 
-# provider "aws" {
-#   region = "ap-southeast-1"
-# }
+provider "aws" {
+  region = "ap-southeast-1"
+}
 
-# resource "aws_instance" "gh-action-instance" {
-#   ami           = "ami-04c913012f8977029" # Replace with a valid AMI ID in your region
-#   instance_type = "t3a.nano"
+resource "aws_instance" "gh-action-instance" {
+  ami           = "ami-04c913012f8977029" # Replace with a valid AMI ID in your region
+  instance_type = "t3a.nano"
 
-#   tags = {
-#     Name = "ec2-github-action-test"
-#   }
-# }
+  tags = {
+    Name = "ec2-github-action-test"
+  }
+}
